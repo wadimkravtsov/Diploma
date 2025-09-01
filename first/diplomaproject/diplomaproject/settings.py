94287ae78b457cd41d218dca39f57e9324feb160
Вадим Кравцов <kravtsov-60@mail.ru>
2025-09-01 20:42:01 +0700

    two

--- first/diplomaproject/diplomaproject/settings.py
+++ first/diplomaproject/diplomaproject/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'realtors.apps.RealtorsConfig',
+    'villages.apps.VillagesConfig',
 ]
 
 MIDDLEWARE = [
