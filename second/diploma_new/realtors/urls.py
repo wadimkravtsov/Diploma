5a39e3ef21901eafee521492e53ffe75b7403c0a
Вадим Кравцов <kravtsov-60@mail.ru>
2025-10-17 21:23:57 +0700

    17.10

--- second/diploma_new/realtors/urls.py
+++ second/diploma_new/realtors/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.realtors, name="realtors"),
     path('realtor/<str:pk>/', views.realtor, name="realtor"),
     path('cottage/', views.create_cottage, name="cottage"),
-
-
+    path('update_cottage/<str:pk>/', views.update_cottage, name="update_cottage"),
+    path('update_cottage/<str:pk>/', views.update_cottage, name="update_cottage"),
+    path('delete_cottage/<str:pk>/', views.delete_cottage, name="delete_cottage"),
 ]
--- second/diploma_new/realtors/views.py
+++ second/diploma_new/realtors/views.py
@@ -34,3 +34,29 @@ def create_cottage(request):
     context = {'form': form}
     return render(request, 'realtors/form-cottage.html', context)
 
+def update_cottage(request,pk):
+    cottage = Cottage.objects.get(id=pk)
+    form = CottageForm(instance=cottage)
+
+    if request.method == "POST":
+        form = CottageForm(request.POST,instance=cottage)
+        print(form)
+        if form.is_valid():
+            form.save()
+            return redirect('realtors')
+
+    context = {'form': form,
+               'cottage': cottage}
+    return render(request, 'realtors/form-cottage.html', context)
+
+def delete_cottage(request, pk):
+    cottage = Cottage.objects.get(id=pk)
+
+    # if request.method == "POST":
+    cottage.delete()
+    return redirect('realtor', pk=cottage.rlt)
+
+    # context = {'object': cottage}
+    # return render(request, 'projects/delete.html', context)
+
+
